9e13020edaa48a7aaf917d3fcee71dae8d653bca
acoustic_jeet <js.jitendra11@gmail.com>
2022-03-09 16:47:33 +0530

    Create generic.kt

---
+++ generic.kt
@@ -0,0 +1,19 @@
+////fun main(args: Array<String>) {
+////    val first: Int = 10
+////    val second: Int = 20
+////
+////    val sum = first + second
+////
+////    println("The sum is: $sum")
+////}
+//
+//fun main() {
+//    val x: String?="Coding Ninjas"
+//    val y: String=x
+//    println(y)
+//}
+fun main() {
+    val k: String = "CodeOn"
+    val y: String?="kt"
+    println(y)
+}
